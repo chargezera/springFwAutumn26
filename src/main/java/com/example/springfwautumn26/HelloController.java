015035e7c7db83b77556b9a4b07715582b914dec
Zhanibek Sagyndyk <zhonik99@gmail.com>
2026-09-12 16:17:45 +0500

    added helloController

--- src/main/java/com/example/springfwautumn26/HelloController.java
+++ src/main/java/com/example/springfwautumn26/HelloController.java
@@ -1,4 +1,21 @@
 package com.example.springfwautumn26;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController("/hello")
+@RequiredArgsConstructor
 public class HelloController {
+
+    private final HelloService helloService;
+
+
+    @GetMapping("/get/{name}")
+    public String getHelloName(@PathVariable String name) { return helloService.giveName(name);}
 }
--- src/main/java/com/example/springfwautumn26/HelloService.java
+++ src/main/java/com/example/springfwautumn26/HelloService.java
@@ -1,4 +1,16 @@
 package com.example.springfwautumn26;
 
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.Objects;
+
+@Service
 public class HelloService {
+
+    public String university = "KBTU";
+
+    public String giveName(String name){
+        return "Your name is " + name  + ". You study at " + university;
+    }
 }
